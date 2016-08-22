122878a353d061e24143b05e08e7add88c676a8d
Andrew Merrell and Brian Dutz <brian@>
2016-08-22 10:16:39 -0700

    add js to use constructor and form data to create list of place info.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,35 @@ function Places(location, landmarks, year, season) {
   this.landmarks = landmarks;
   this.year = year;
   this.season = season;
+
 }
+
+// Places.prototype.fullInfo = function() {
+//   return this.location + " " + this.landmarks + " " + this.year + " " + this.season;
+// }
 
 //UI Logic
 $(document).ready(function() {
   $("form").submit(function(event){
-
-
     event.preventDefault();
+
+    var inputtedLocation = $("input#location").val();
+    var inputtedLandmarks = $("input#landmarks").val();
+    var inputtedYear = $("input#year").val();
+    var inputtedSeason = $("input#season").val();
+
+    var newPlaces = new Places(inputtedLocation, inputtedLandmarks, inputtedYear, inputtedSeason);
+
+  $("#newPlaces").append("<p><span class='places'>" + newPlaces.location + "</span></p>");
+
+    $(".places").last().click(function() {
+    $("#show-places").show();
+    $("#show-places h2").text(newPlaces.location);
+
+    $(".landmarks").text(newPlaces.landmarks);
+    $(".year").text(newPlaces.year);
+    $(".season").text(newPlaces.season);
+
+   });
   });
 });
